b8a74328a1cc420adad0dc01155ae019c3733bca
SergDerbst <sergio.weigel@gmail.com>
2021-06-11 08:20:14 +0200

    added typescript compile

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import {
+  Patchbay,
+  PatchCord,
+  SocketType,
+  Socket,
+  SocketFactory,
+  SocketProvider,
+  AuthSocket,
+  DataSocket,
+  ErrorSocket,
+  LogicSocket,
+  RouteSocket,
+  StoreSocket
+} from "./patchbay";
+
+export {
+  Patchbay,
+  PatchCord,
+  SocketType,
+  Socket,
+  SocketFactory,
+  SocketProvider,
+  AuthSocket,
+  DataSocket,
+  ErrorSocket,
+  LogicSocket,
+  RouteSocket,
+  StoreSocket
+}
--- src/patchbay.ts
+++ src/patchbay.ts
@@ -40,54 +40,54 @@ export abstract class Patchbay<
 	R extends RouteSocket,
 	S extends StoreSocket> {
 	protected readonly sockets: { [key: string]: Socket };
-	
+
 	protected constructor() {
 		this.sockets = {};
 	}
-	
+
 	protected addSocket = (name: string, socket: Socket): void => {
 		this.sockets[name] = socket;
 	}
-	
+
 	protected connect = (name: string): Socket => {
 		return this.sockets[name];
 	}
-	
+
 	auth: SocketProvider<A> = (socket?: A): A => {
 		if (socket) {
 			this.sockets[SocketType.Auth] = socket;
 		}
 		return this.sockets[SocketType.Auth] as A;
 	}
-	
+
 	data: SocketProvider<D> = (socket?: D): D => {
 		if (socket) {
 			this.sockets[SocketType.Data] = socket;
 		}
 		return this.sockets[SocketType.Data] as D;
 	}
-	
+
 	error: SocketProvider<E> = (socket?:E): E => {
 		if (socket) {
 			this.sockets[SocketType.Error] = socket;
 		}
 		return this.sockets[SocketType.Error] as E;
 	}
-	
+
 	logic: SocketProvider<L> = (socket?: L): L => {
 		if (socket) {
 			this.sockets[SocketType.Logic] = socket;
 		}
 		return this.sockets[SocketType.Logic] as L;
 	}
-	
+
 	route: SocketProvider<R> = (socket?: R): R => {
 		if (socket) {
 			this.sockets[SocketType.Router] = socket;
 		}
 		return this.sockets[SocketType.Router] as R;
 	}
-	
+
 	store: SocketProvider<S> = (socket?: S): S => {
 		if (socket) {
 			this.sockets[SocketType.Store] = socket;
